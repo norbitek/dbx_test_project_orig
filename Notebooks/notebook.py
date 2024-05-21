ec34806b7e87542a474c60ff9a0711fd4d63ceda
norbitek <norbitek>
2024-05-21 13:54:38 +0000

    Change notebook type

--- Notebooks/notebook.py
+++
@@ -1,5 +0,0 @@
-# Databricks notebook source
-from databricks.connect import DatabricksSession
-spark = DatabricksSession.builder.getOrCreate()
-df = spark.read.table("samples.nyctaxi.trips")
-display(df)
